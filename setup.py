b1f838fbd69c644a6f02ac00aae9829b09043ba5
Ralph Bean <rbean@redhat.com>
2013-10-26 22:37:51 -0400

    Reorganize into a python module for fedora-infra/tahrir#238.

---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+f = open('README.md')
+long_description = f.read().strip()
+f.close()
+
+
+requires = []
+
+version = '0.1.0'
+
+setup(
+    name='fossrit_tahrir_theme',
+    version=version,
+    description="FOSSRIT theme for the tahrir open badges webapp",
+    long_description=long_description,
+    author='David Gay',
+    author_email='oddshocks@riseup.net',
+    url='http://github.com/ralphbean/ansi2html/',
+    license='GPLv3+',
+    classifiers=[],
+    install_requires=requires,
+    packages=['fossrit_tahrir_theme'],
+    include_package_data=True,
+    zip_safe=False,
+)
